29eca1f0dc04e68b5ac0ed901e94c13678c1bb8d
Jawann <jawann@gmail.com>
2017-12-10 22:43:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
-html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
-
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
 end
+
+
+create_project_hash
